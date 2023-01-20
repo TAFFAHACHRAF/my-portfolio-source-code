355afa0d062a1e3ada13da790d42b5a9cf71ae68
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2023-01-20 21:00:07 +0100

    Add files via upload

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+import { BsYoutube } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
@@ -14,8 +14,8 @@ function ProjectCards(props) {
           {props.description}
         </Card.Text>
         <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
+          <BsYoutube /> &nbsp;
+          {props.isBlog ? "Youtube" : "Youtube"}
         </Button>
         {"\n"}
         {"\n"}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import java1 from "../../Assets/Projects/java-1.png";
-import drupal1 from "../../Assets/Projects/drupal-1.png";
-import node1 from "../../Assets/Projects/nodeJs-1.png";
-import node2 from "../../Assets/Projects/nodeJs-2.png";
-import node3 from "../../Assets/Projects/nodeJs-3.png";
+import java1 from "../../Assets/Projects/nodeJs-1.png";
+import drupal1 from "../../Assets/Projects/nodeJs-2.png";
+import node1 from "../../Assets/Projects/nodeJs-3.png";
+import node2 from "../../Assets/Projects/drupal-1.png";
+import node3 from "../../Assets/Projects/java-1.png";
 
 function Projects() {
   return (
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={java1}
               isBlog={false}
-              title="Tasks management project"
-              description="The goal of this project is the development of a JAVA application to manage the projects. This application will allow you to manage project tasks, resources, and displays the schedule in a Gantt chart."
-              ghLink="https://github.com/TAFFAHACHRAF/Tasks-management-project"
-              demoLink="https://github.com/TAFFAHACHRAF/Tasks-management-project"
+              title="Blooger"
+              description="In this project I developed a Blog whose Backend and Frontend parts are developed and tested separately. The backend will work as a JSON API."
+              ghLink="https://www.youtube.com/channel/UCxoar6KFc6u3beif_e4y3lw"
+              demoLink="https://github.com/TAFFAHACHRAF/taffah-projet-web"
             />
           </Col>
 
@@ -36,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={drupal1}
               isBlog={false}
-              title="ENSET NEWS"
-              description="The objective of this project is the realization of a Web Application using DRUPAL CMS to manage the news of ENSET MOHAMMEDIA."
-              ghLink="https://taffah.123.ma"
-              demoLink="https://taffah.123.ma"
+              title="GUIDE DERB OMAR"
+              description="The objective of this project is the realization of a full stack web application in the form of a reservation system for commercial consultations, and a directory of product source links, simple and easy to use."
+              ghLink="https://www.youtube.com/channel/UCxoar6KFc6u3beif_e4y3lw"
+              demoLink="https://github.com/TAFFAHACHRAF/Guide-Derb-Omar-App"
             />
           </Col>
 
@@ -47,10 +47,10 @@ function Projects() {
             <ProjectCard
               imgPath={node1}
               isBlog={false}
-              title="Blooger"
-              description="In this project I developed a Blog whose Backend and Frontend parts are developed and tested separately. The backend will work as a JSON API."
-              ghLink="https://github.com/TAFFAHACHRAF/taffah-projet-web"
-              demoLink="https://github.com/TAFFAHACHRAF/taffah-projet-web" 
+              title="My portfolio"
+              description="In this portfolio i have presented my technical skills, and my professional and academic projects that I have carried out during my career.."
+              ghLink="https://taffah-achraf.netlify.app"
+              demoLink="https://taffah-achraf.netlify.app" 
             />
           </Col>
 
@@ -58,10 +58,10 @@ function Projects() {
             <ProjectCard
               imgPath={node2}
               isBlog={false}
-              title="GUIDE DERB OMAR"
-              description="The objective of this project is the realization of a full stack web application in the form of a reservation system for commercial consultations, and a directory of product source links, simple and easy to use, which firstly contains a public interface which presents the GUIDE DERB OMAR companies, and allowing sales companies to create an account with a type of subscription to be determined."
-              ghLink="https://github.com/TAFFAHACHRAF/Guide-Derb-Omar-App"
-              demoLink="hhttps://github.com/TAFFAHACHRAF/Guide-Derb-Omar-App"
+              title="ENSET NEWS"
+              description="The objective of this project is the realization of a Web Application using DRUPAL CMS to manage the news of ENSET MOHAMMEDIA."
+              ghLink="https://www.youtube.com/watch?v=DuMX5UOq6VY&t=45s"
+              demoLink="https://taffah.123.ma"
             />
           </Col>
 
@@ -70,10 +70,10 @@ function Projects() {
             <ProjectCard
               imgPath={node3}
               isBlog={false}
-              title="My portfolio"
-              description="In this portfolio i have presented my technical skills, and my professional and academic projects that I have carried out during my career.."
-              ghLink="https://github.com/TAFFAHACHRAF/my-portfolio-taffah"
-              demoLink="https://github.com/TAFFAHACHRAF/my-portfolio-taffah"
+              title="Tasks management project"
+              description="The goal of this project is the development of a JAVA application to manage the projects. This application will allow you to manage project tasks, resources, and displays the schedule in a Gantt chart."
+              ghLink="https://www.youtube.com/channel/UCxoar6KFc6u3beif_e4y3lw"
+              demoLink="https://github.com/TAFFAHACHRAF/Tasks-management-project"
             />
           </Col>
 
